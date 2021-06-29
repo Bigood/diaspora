c5ca68ad8d3434f9be0d5530e4c5ec197e14c890
Maen Juganaikloo <mjuganaikloo+github@gmail.com>
2021-06-29 16:09:16 +0000

    Extendend string field limits to activities and such

--- db/migrate/20210519095550_add_cartotalents_to_profiles.rb
+++ db/migrate/20210519095550_add_cartotalents_to_profiles.rb
@@ -2,11 +2,11 @@ class AddCartotalentsToProfiles < ActiveRecord::Migration[5.2]
   def change
     add_column :profiles, :carto_latitude, :string
     add_column :profiles, :carto_longitude, :string
-    add_column :profiles, :carto_etablissement, :string
+    add_column :profiles, :carto_etablissement, :string, :limit => 1024
     add_column :profiles, :carto_user_type, :string
-    add_column :profiles, :carto_technics, :string
-    add_column :profiles, :carto_activites, :string
-    add_column :profiles, :carto_methods, :string
+    add_column :profiles, :carto_technics, :string, :limit => 2048
+    add_column :profiles, :carto_activites, :string, :limit => 2048
+    add_column :profiles, :carto_methods, :string, :limit => 2048
     add_column :profiles, :carto_id, :string
   end
 end
